b84805b4be651147a05229fa0d37d276696d6cd8
Anton Balashov <balashov.anton.1983@gmail.com>
2023-06-16 21:16:34 +0300

    Decide how to test UserList, add tests pseudo code

---
+++ src/components/UsersList/UserList.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import { UserList } from './UsersList';
+
+test('render one row per user', () => {
+  // TODO:
+  // 1. Render the UserList component
+  // 2. Manipulate it to find all the rows in the table
+  // 3. Assert we have correct number of rows in the table
+});
+
+test('render the email and name of each user', () => {
+  // TODO:
+  // 1. Render the UserList component
+  // 2. Manipulate it to get the array of all the rows in the table
+  // 3. Assert that users array mapped from rows corresponds users prop
+});
