754b722daab69e3f72c5d1327e0fdbbb828005e4
Anton Balashov <balashov.anton.1983@gmail.com>
2024-10-04 23:41:04 +0300

    Extract component render

--- src/components/UsersList/UserList.test.js
+++ src/components/UsersList/UserList.test.js
@@ -1,8 +1,7 @@
 import { render, screen, within } from '@testing-library/react';
 import { UsersList } from './UsersList';
 
-test('render one row per user', () => {
-  // 1. Render the UsersList component
+function renderComponent() {
   const users = [
     { name: 'Jane', email: 'jane@jane.com' },
     { name: 'Sam', email: 'sam@sam.com' },
@@ -10,28 +9,24 @@ test('render one row per user', () => {
 
   render(<UsersList users={users} />);
 
-  // 2. Find all the rows in the table
+  return { users };
+}
+
+test('render one row per user', () => {
+  renderComponent();
+
   const rows = within(screen.getByTestId('users')).getAllByRole('row');
 
-  // 3. Assert we have correct number of rows in the table
   expect(rows).toHaveLength(2);
 });
 
 test('render the email and name of each user', () => {
-  // 1. Render the UsersList component
-  const users = [
-    { name: 'Jane', email: 'jane@jane.com' },
-    { name: 'Sam', email: 'sam@sam.com' },
-  ];
-
-  render(<UsersList users={users} />);
+  const { users } = renderComponent();
 
   for (const user of users) {
-    // 2. Manipulate it to find the name and email of each user
     const name = screen.getByRole('cell', { name: user.name });
     const email = screen.getByRole('cell', { name: user.email });
 
-    // 3. Assert that name and email of each user are visible on the screen
     expect(name).toBeInTheDocument();
     expect(email).toBeInTheDocument();
   }
