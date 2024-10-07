6eb4b9e63a455e05be264bd43061b40079be20e0
Anton Balashov <balashov.anton.1983@gmail.com>
2024-10-07 15:49:04 +0300

    Find and manipulate app component in app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,25 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
 import App from './App';
 
-test('can receive a new user and show it on a list', () => {
+test('can receive a new user and show it on a list', async () => {
   // 1. Render app component.
   render(<App />);
 
   // 2. Find name & email inputs, click on them, type in them, click the button to submit the form.
+  const nameInput = screen.getByRole('textbox', { name: /name/i });
+  const emailInput = screen.getByRole('textbox', { name: /email/i });
+  const button = screen.getByRole('button');
+
+  await user.click(nameInput);
+  await user.keyboard('jane');
+
+  await user.click(emailInput);
+  await user.keyboard('jane@jane.com');
+
+  await user.click(button);
+
+  screen.debug();
+
   // 3. Make sure we can find the new name & email in the table.
 });
